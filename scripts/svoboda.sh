65ed53a2b432a6dce4cbdc234dd70ab22f65964b
Yaroslav Halchenko <debian@onerussian.com>
2020-02-04 18:30:05 -0500

    need to "" all the globs or zsh pukes

--- scripts/svoboda.sh
+++ scripts/svoboda.sh
@@ -1,2 +1,9 @@
 #!/bin/zsh
-( outd="$PWD"; cd /mnt/btrfs/datasets/datalad/crawl/labs/svoboda; for d in *(/); do /usr/bin/find "$d" -iname *nwb \! -ipath */objects/* -print0 | xargs -0 -r dandi ls -f json >| $outd/svoboda-$d.json ; done; )
+
+outd="$PWD"; 
+
+cd /mnt/btrfs/datasets/datalad/crawl/labs/svoboda; 
+
+for d in *(/); do 
+    /usr/bin/find "$d" -iname "*.nwb" \! -ipath "*/objects/*" -print0 | xargs -0 -r dandi ls -f json >| $outd/svoboda-$d.json ; 
+done;
